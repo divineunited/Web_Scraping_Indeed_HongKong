ca000f04e522eb2f52c01cf643eda17fee08ff65
DivineUnited <DanielDukeVu@outlook.com>
2019-03-02 08:20:14 +0800

    data mining complete

--- indeedSpider/indeedSpider/items.py
+++ indeedSpider/indeedSpider/items.py
@@ -6,13 +6,6 @@
 # https://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
-from scrapy import Item, Field
-
-class IndeedspiderItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
-
 
 class Article(scrapy.Item):
     # define the fields for your item here like
--- indeedSpider/indeedSpider/spiders/article.py
+++ indeedSpider/indeedSpider/spiders/article.py
@@ -1,23 +1,31 @@
 # -*- coding: utf-8 -*-
-from scrapy.spiders import CrawlSpider, Rule
-from wikiSpider.items import Article
-from scrapy.linkextractors import LinkExtractor
+import scrapy
+from scrapy.linkextractor import LinkExtractor
+from scrapy.spiders import Rule, CrawlSpider
+from indeedSpider.items import Article
 
-class ArticleSpider(CrawlSpider): 
 
-    # The code in the top part of the class defines the crawling logic essentially which page to visits
-    name = "article" # name - is a string which we can use to call our spider on the command line.
+class ArticleSpider(CrawlSpider):
+    # The name of the spider
+    name = "article"
 
-    allowed_domains = ["en.wikipedia.org"] # is a list of domains which we'll let our spider vist.
+    # The domains that are allowed (links to other domains are skipped)
+    allowed_domains = ["indeed.hk"]
 
-    start_urls = ["http://en.wikipedia.org/wiki/Python_%28programming_language%29"] # url of the page or pages to start on.
-    
-    # The rule object takes a LinkExtractor and two other arguments. The callback argument takes a function which will be called on each page to excute our parsing logic (which is our parse_item function defined below). And the final argument follow which is a boolean which specifies if links should be followed from each response extracted, it defualts to True.
-    rules = [ 
-        #Link extractor uses a regex to define which links to follow - this will match the links we wish to extract.
-        Rule(LinkExtractor(allow=('(/wiki/)((?!:).)*$'),), callback="parse_item", follow=True)
+    # The URLs to run on. The site maxes out at page start page 990. 
+    start_urls = [f"https://www.indeed.hk/jobs?q=data+science&l=Hong+Kong&start={i}" for i in range(0, 1000, 10)]
+
+    # This spider has one rule: extract all (unique and canonicalized) links, follow them and parse them using the parse_item method
+    rules = [
+        Rule(
+            LinkExtractor(
+                canonicalize=True,
+                unique=True
+            ),
+            follow=True,
+            callback="parse_item"
+        )
     ]
-
 
     #Spiders can take custom settings, these are higher priority then the setting.py - contains additional setting specific to this spider.
     custom_settings = {
@@ -25,18 +33,42 @@ class ArticleSpider(CrawlSpider):
         'DOWNLOAD_DELAY' : 0.25
     }
 
-    # parse_item is the parsing logic, what items in that page are we interested in.
+    # Method which starts the requests by visiting all URLs specified in start_urls
+    def start_requests(self):
+        for url in self.start_urls:
+            yield scrapy.Request(url, callback=self.parse, dont_filter=True)
+
+
     def parse_item(self, response):
-        '''When it goes to a new website, this is what I do to scrape this webpage.'''
 
         # creating a new instance of our Article() object that will store the data that inherits from Item parent object defined in our framework (items.py)
         item = Article() 
 
         # passes a response into this from our crawl, and here we get the title
-        title = response.css('#firstHeading::text').extract_first()
+        title = response.xpath('//head/title/text()').getall()
         # get the content
-        content =  response.css('#toc *::text').extract()
-        print("Title is: "+title)
-        item['title'] = title
+        content = response.xpath('//div/ul/li/text()').getall()
+
+        # This is my flag - if more than 25% of all the characters are white space, we have a problem, dump the content:
+        space_percent = str(content).count(' ') / len(str(content))
+        if space_percent > .25:
+            content = []
+        # another flag, if the content size is less than this, it must be nothing:
+        elif len(str(content)) < 30:
+            content = []
+        # another flag, removing terms of service link:
+        elif title[0] == "Cookies, Privacy and Terms of Service | Indeed.hk":
+            content = []
+        
+        # get the string of the first title inside the list of strings
+        item['title'] = title[0]
+        # passing content to our item
         item['content'] = content
-        return item
+        
+        # Tracing
+        print("Title is: ", title[0])
+        print("Content is: ", content)
+
+        # only return content if we didn't dump it
+        if content:
+            return item
